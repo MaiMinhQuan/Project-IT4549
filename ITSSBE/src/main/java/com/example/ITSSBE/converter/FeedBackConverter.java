5e5ec11f82ede0a5f89530c6a2fba618542cec99
Vũ Minh Quân <Quan.VM225910@sis.hust.edu.vn>
2025-06-01 22:27:15 +0700

    converter_v1

---
+++ ITSSBE/src/main/java/com/example/ITSSBE/converter/FeedBackConverter.java
@@ -0,0 +1,60 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.example.ITSSBE.converter;
+
+import com.example.ITSSBE.dto.FeedBackDTO;
+import com.example.ITSSBE.entity.FeedBack;
+import com.example.ITSSBE.entity.User;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FeedBackConverter {
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public FeedBackConverter() {
+    }
+
+    public FeedBackDTO toDTO(FeedBack feedback) {
+        FeedBackDTO feedBackDTO = (FeedBackDTO)this.modelMapper.map(feedback, FeedBackDTO.class);
+        feedBackDTO.setUser_id(feedback.getBy_user().getId());
+        return feedBackDTO;
+    }
+
+    public FeedBack toEntity(FeedBackDTO feedBackDTO, User user) {
+        FeedBack feedBack = (FeedBack)this.modelMapper.map(feedBackDTO, FeedBack.class);
+        feedBack.setBy_user(user);
+        return feedBack;
+    }
+}
+
+//package com.example.ITSSBE.converter;
+//
+//import com.example.ITSSBE.dto.FeedBackDTO;
+//import com.example.ITSSBE.entity.FeedBack;
+//import com.example.ITSSBE.entity.User;
+//import org.modelmapper.ModelMapper;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class FeedBackConverter {
+//    @Autowired
+//    private ModelMapper modelMapper;
+//    public FeedBackDTO toDTO(FeedBack feedback){
+//        FeedBackDTO feedBackDTO = modelMapper.map(feedback, FeedBackDTO.class);
+//        feedBackDTO.setUser_id(feedback.getBy_user().getId());
+//        return feedBackDTO;
+//    }
+//
+//    public FeedBack toEntity(FeedBackDTO feedBackDTO, User user) {
+//        FeedBack feedBack = modelMapper.map(feedBackDTO, FeedBack.class);
+//        feedBack.setBy_user(user);
+//        return feedBack;
+//    }
+//}
